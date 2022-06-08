4856bbf32d3f4503a3a0dfc0cf9398e413eb258f
Nick Shore <NickShore111@gmail.com>
2022-06-08 01:49:09 -0700

    Initial commit

---
+++ src/main/java/com/perscholas/astrodia/AstrodiaApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.astrodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrodiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/astrodia/AstrodiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.astrodia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrodiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
