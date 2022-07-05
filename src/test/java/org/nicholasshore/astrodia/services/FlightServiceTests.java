0a4d0e20f0e09f00c91d051b13db29b956097aa0
Nick Shore <NickShore111@gmail.com>
2022-07-05 11:09:45 -0700

    all query tests pass, added parameterized test to FlightServiceTests

--- src/test/java/org/nicholasshore/astrodia/services/FlightServiceTests.java
+++ src/test/java/org/nicholasshore/astrodia/services/FlightServiceTests.java
@@ -3,6 +3,7 @@ package org.nicholasshore.astrodia.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.nicholasshore.astrodia.models.Flight;
@@ -10,7 +11,9 @@ import org.nicholasshore.astrodia.models.Spaceliner;
 import org.nicholasshore.astrodia.util.AstrodiaData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -20,6 +23,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @SpringBootTest
+@Transactional //handle Hibernate.LazyInitializeException
+@PropertySource("classpath:application-mariadb.properties")
 public class FlightServiceTests {
     String[] spacelinerList = null;
     String[] shuttleList = null;
@@ -32,22 +37,37 @@ public class FlightServiceTests {
     @Autowired
     FlightService flightService;
 
+
     @ParameterizedTest
-    @CsvSource({
-            "MO, ES, 07/04/2022, 07/08/2022",
-            "MA, EO, 07/06/2022, 07/12/2022"
-    })
-    public void regionRoundtripSearchTest(String departureRegion, String arrivalRegion, String departureDate, String arrivalDate) {
-        List<Flight> departures = flightService.searchByRegionDepartureAndDateRange(departureRegion, arrivalRegion, departureDate, arrivalDate);
-        List<Flight> returns = flightService.searchByRegionArrivalAndDateRange(departureRegion, arrivalRegion, departureDate,arrivalDate);
+    @MethodSource("regionRoundtripParameterProvider")
+    public void regionRoundtripSearchTest(String departureRegionId, String arrivalRegionId, String departureDate, String arrivalDate) {
+        List<Flight> departures = flightService.searchByRegionDepartureAndDateRange(departureRegionId, arrivalRegionId, departureDate, arrivalDate);
+        List<Flight> returns = flightService.searchByRegionArrivalAndDateRange(departureRegionId, arrivalRegionId, departureDate,arrivalDate);
 
         departures.forEach(flight -> {
-            assertThat(flight.getLaunchPad().getPort().getRegion().getId()).isEqualTo(departureRegion);
+            assertThat(flight.getLaunchPad().getPort().getRegion().getId()).isEqualTo(departureRegionId);
             assertThat(flight.getDeparting()).isAfterOrEqualTo(new Date());
+
+            assertThat(flight.getArrivalPad().getPort().getRegion().getId()).isEqualTo(arrivalRegionId);
+        });
+
+        returns.forEach(flight -> {
+            assertThat(flight.getLaunchPad().getPort().getRegion().getId()).isEqualTo(departureRegionId);
+            assertThat(flight.getDeparting()).isAfterOrEqualTo(new Date());
+
+            assertThat(flight.getArrivalPad().getPort().getRegion().getId()).isEqualTo(arrivalRegionId);
         });
     }
+
+    static Stream<Arguments> regionRoundtripParameterProvider() {
+        return Stream.of(
+                arguments("MO", "ES", "07/08/2022", "07/14/2022"),
+                arguments("MA", "EO", "07/08/2022", "07/14/2022"),
+                arguments("ES", "MA", "07/08/2022", "07/16/2022")
+        );
+    }
     @Test
-    public void getAllFlightsUsingZeroSelectionCriteria() {
+    public void getAllFlightsUsingZeroSelectionCriteriaTest() {
         List<Flight> actualFlights = flightService.findAll();
 
         List<Flight> expectedFlights = flightService.findFlightsBySelectionCriteria(
